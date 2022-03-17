3469cbbd5999d2f18ae64842661bc1ad0415af9c
StashCurrie <stashcurrie@gmail.com>
2022-03-16 22:48:43 -0700

    First Commit
    Muffins or Cupcakes. 
    SimpliLearn 9 hr course

---
+++ muffins.py
@@ -0,0 +1,11 @@
+# packages
+import numpy as np
+#used for dataframes (kinda like excell spreadsheets)
+import pandas as pd
+
+from sklearn import svm
+
+#visualizing data
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set(font_scale = 1.2)
+
